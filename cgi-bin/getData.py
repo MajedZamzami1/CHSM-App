5446711ce6674b09a42b58cae89d43b00a424a83
YahyaJ4 <109200533+YahyaJ4@users.noreply.github.com>
2022-08-02 19:12:41 -0500

    edit - yahya

--- cgi-bin/getData.py
+++ cgi-bin/getData.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+from cProfile import label
 import cgi
 import cgitb
 from xml.etree.ElementTree import tostring
@@ -24,7 +25,7 @@ def Form2Dict():
                 v = str( F.getvalue(k) )
         D[k] = v
     return D
-
+    
 D = Form2Dict()
 
 D['status'] = True
@@ -44,13 +45,23 @@ def add():
     # Open file, read into local dict variable, edit.
     with open(filename, 'r') as json_file:
         json_data = json.load(json_file)
+    
+    #D['_latlng'] is in the form: LatLng(30.351819, -97.700206)
+    latlng = D['_latlng'].replace('LatLng' , '')
+    latlng = latlng.strip('(')
+    latlng = latlng.strip(')')
+    latlng = latlng.split(',')
+    latlng[1] = latlng[1].strip()
+    #resulted form ["30.351819", "-97.700206"]
 
     # Add riders to the database
-    user = {"id": D["id"], "name": D["name"], "surname": D["surname"], "birthdate": D["birthdate"], 'waiver': D['waiver']}
+    user = {"id": D["id"], "name": D["name"], "surname": D["surname"], "birthdate": D["birthdate"], "trip" : D['trip'] , "location":latlng, 'waiver': D['waiver']}
     temp_rider = False
     for rider in json_data["riders"]:
         if rider['id'] == user['id']:
             temp_rider = True
+            rider['trip'] = user['trip']
+            rider['location'] = user['location']
     if temp_rider == False:
         json_data["riders"].append(
             user
@@ -64,9 +75,9 @@ def add():
                 if t[0] == D['id']:
                     temp = True
             if temp == False:
-                trip[D['trip']].append([D['id'] , D['location']])
+                trip[D['trip']].append([D['id'] , latlng])
         else:
-            trip[D['trip']] = [[D['id'] , D['location']]]
+            trip[D['trip']] = [[D['id'] , latlng]]
     
     # Write local dict (edited) back to json file.
     with open(filename, "w") as json_file:
